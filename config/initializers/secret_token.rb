20be9cb2b8a5e6445b6d23a6a0c516d787d7bca4
Sanjay Murti <sanjay.murti@gmail.com>
2014-03-12 15:14:34 -0400

    Initial commit of rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blt::Application.config.secret_key_base = '073934bc78756b54ae4b18c8f4c3bf7e34b375ae2b6724631b9936602ff39d8a8c1f0dcff136adb71abae7b34403e6e73447348d765ce519048337b95969e732'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Blt::Application.config.session_store :cookie_store, key: '_blt_session'
